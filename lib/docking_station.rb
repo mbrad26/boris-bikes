6f0efabe954a7a17962797678dad1af553740a41
mbrad26 <mbrad26@gmail.com>
2020-04-16 16:01:08 +0000

    refactored station capacity code into private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,23 @@ class DockingStation
   end
   
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes[0]
   end
   
   def dock_bike(bike)
-    fail 'Station is full' if @bikes.length == 20
+    fail 'Station is full' if full?
     @bikes << bike
+  end
+  
+  private
+  
+  def full?
+    @bikes.length >= 20
+  end
+  
+  def empty?
+    @bikes.length == 0
   end
 end
 
