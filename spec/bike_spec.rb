fd8a044644a8fe941ac4cdaccaed49f04fb55e22
mbrad26 <mbrad26@gmail.com>
2020-04-16 14:11:16 +0000

    added tests for working? and release_bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,11 @@ describe Bike do
   
   it { is_expected.to respond_to(:working?) }
   
+  describe '#working?' do
+    context 'when bike is in working condition' do
+      it 'returns true' do
+        expect(subject.working?).to be true
+      end
+    end
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+  
+  describe '#release_bike' do
+    it 'returns a bike' do
+      expect(subject.release_bike).to be_a_kind_of(Bike)
+    end
+  end
 end
