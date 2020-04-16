cf71ffac6d2b3d1034ec6eb8cc85480f0aadd85c
mbrad26 <mbrad26@gmail.com>
2020-04-16 15:52:17 +0000

    updated docking_station to receive 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,19 @@
 class DockingStation
   
-  attr_reader :bike
+  attr_reader :bikes
+  
+  def initialize
+    @bikes = []
+  end
   
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes[0]
   end
   
   def dock_bike(bike)
-    fail 'Station is full' if @bike
-    @bike = bike
+    fail 'Station is full' if @bikes.length == 20
+    @bikes << bike
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
   
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
   
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   
   describe '#release_bike' do
     it 'returns a bike' do
@@ -25,16 +25,21 @@ describe DockingStation do
     it 'docks a bike' do
       bike = Bike.new
       
-      expect { subject.dock_bike(bike) }.to change(subject, :bike).to eq bike
+      expect { subject.dock_bike(bike) }.to change(subject, :bikes).to include(bike)
     end
     
     it 'raises an error when there is already in the station' do
-      bike = Bike.new
-      subject.dock_bike(bike)
+      20.times { subject.dock_bike(Bike.new) }
       
       another_bike = Bike.new
       
       expect { subject.dock_bike(another_bike) }.to raise_error 'Station is full'
+    end
+  end
+  
+  describe '#bikes' do
+    it 'returns an array' do
+      expect(subject.bikes).to be_a_kind_of(Array)
     end
   end
 end
