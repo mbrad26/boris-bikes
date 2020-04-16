36d442875f3f56fc11f210ab6805f97761164d45
mbrad26 <mbrad26@gmail.com>
2020-04-16 14:50:47 +0000

    added bike docking and bike storage instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,13 @@
 class DockingStation
   
+  attr_reader :bike
+  
   def release_bike
     Bike.new
   end
+  
+  def dock_bike(bike)
+    @bike = bike
+  end
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,21 @@ require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  
+  it { is_expected.to respond_to(:bike) }
+  
   describe '#release_bike' do
     it 'returns a bike' do
       expect(subject.release_bike).to be_a_kind_of(Bike)
+    end
+  end
+  
+  describe '#dock_bike' do
+    it 'docks a bike' do
+      bike = Bike.new
+      
+      expect { subject.dock_bike(bike) }.to change(subject, :bike).to eq bike
     end
   end
 end
